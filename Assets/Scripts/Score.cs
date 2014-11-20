d9d19838b14a9d6cc18bab368c4214812c956064
tsunefuka-k <tsunefuka-k@klab.com>
2014-11-20 21:02:43 +0900

    スコア・ハイスコアの表示領域作成

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour 
+{
+	// スコアを表示するGUIText
+	public GUIText scoreGUIText;
+
+	// ハイスコアを表示するGUIText
+	public GUIText highScoreGUIText;
+
+	private int score;
+	private int highScore;
+
+	// PlayerPrefsで保存するためのキー
+	private string highScoreKey = "highScore";
+
+	void Start () {
+		this.Initialize ();
+	}
+	
+	void Update () {
+		this.scoreGUIText.text = this.score.ToString ();
+		this.highScoreGUIText.text = "HighScore : " + this.highScore.ToString ();
+	}
+
+	private void Initialize ()
+	{
+		this.score = 0;
+		this.highScore = PlayerPrefs.GetInt (this.highScoreKey, 0);
+	}
+
+	public void AddPoint (int point)
+	{
+		this.score += point;
+	}
+
+	public void Save ()
+	{
+		// ハイスコアをPlayerPrefsに保存
+		PlayerPrefs.SetInt (this.highScoreKey, highScore);
+		PlayerPrefs.Save ();
+
+		Initialize ();
+	}
+}
