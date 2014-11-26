9041bb85f546ec3b57cfb561ca54027c8dc8a26b
tsunefuka-k <tsunefuka-k@klab.com>
2014-11-26 19:34:59 +0900

    アイテムの落下処理追加

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -3,5 +3,18 @@ using System.Collections;
 
 public class Item : MonoBehaviour 
 {
+	public float speed = 0.5f;
+
 	public int powerup = 1;
+
+	void Start ()
+	{
+		// 移動処理
+		this.Move (this.transform.up * -1);
+	}
+
+	protected void Move (Vector2 direction)
+	{
+		this.rigidbody2D.velocity = direction * this.speed;
+	}
 }
